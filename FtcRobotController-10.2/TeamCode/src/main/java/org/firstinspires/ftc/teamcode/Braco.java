1a702a5aa832f884e0dc0d6dc5f4d513c91dcd2f
Itallo Mendes <itallomendes1@gmail.com>
2025-06-05 15:33:14 -0300

    Classe do braço foi criada

--- FtcRobotController-10.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Braco.java
+++ FtcRobotController-10.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Braco.java
@@ -1,7 +1,20 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
+@Disabled
 public class Braco {
     DcMotor modularBraco;
+
+    public Braco(HardwareMap hardwareMap) {
+        modularBraco = hardwareMap.dcMotor.get("Braco");
+    }
+
+    public void moverBraco(int posicao) {
+        modularBraco.setTargetPosition(posicao);
+        modularBraco.setPower(0.7);
+        modularBraco.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
 }
--- FtcRobotController-10.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DIASDELUTA.java
+++ FtcRobotController-10.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DIASDELUTA.java
@@ -4,21 +4,23 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
 public class DIASDELUTA extends LinearOpMode {
-
-
     @Override
     public void runOpMode() throws InterruptedException {
         SampleMecanumDrive robo = new SampleMecanumDrive(hardwareMap);
+        Braco bracoColeta = new Braco(hardwareMap);
         Pose2d posicaoinicial = new Pose2d(new Vector2d(0,0), Math.toRadians(0));
         robo.setPoseEstimate(posicaoinicial);
 
         Trajectory Score = robo.trajectoryBuilder(posicaoinicial)
                 .build();
         waitForStart();
+
+        bracoColeta.moverBraco(550);
 
         robo.followTrajectory(Score);
     }
